842db01a7243a8fa4600efd0a70a4eb0ab77fc55
Dmitry Strunin <m-kris@narod.ru>
2020-06-07 22:37:15 +0300

    Сделал обьект Доска, функции расставления фигур и очистку доски.

--- under_attack.py
+++ under_attack.py
@@ -1,25 +1,46 @@
-class Chessboard:
+class Chessboard():
 
     FIRST_LETTER_CODE = ord('A')
+    LETTERS_CNT = 26
 
     def __init__(self, size = 8):
-        self.SIZE = min(size, 26)
+        self.SIZE = min(size, self.LETTERS_CNT)
         self.letter_idx = [
             chr(ucode + self.FIRST_LETTER_CODE)
             for ucode in range(self.SIZE)
         ]
-        board_str = [
-            '0'
-            for i in range(self.SIZE)
-        ]
-        self.board = []
-        for j in range(self.SIZE):
-            self.board.append(board_str)
-            
+        self._board_clear()
+        self.figures_card = dict()
+
+    def _board_clear(self):
+         self.board = [['0' for i in range(self.SIZE)] for j in range(self.SIZE)]
+
+    def put_figure(self, Figure, place:str):
+        '''Проверка попадания в поле. Возвращает абстрактные координаты'''
+        y = int(place[1]) - 1
+        if (
+            (len(place) != 2) or
+            (place[0] not in self.letter_idx) or
+            y not in range(len(self.letter_idx))
+        ):
+            return [None, None]
+
+        x = self.letter_idx.index(place[0])
+        #    if max(x, y) >= self.BOARD_SIZE:
+        #     return [None, None]
+
+        self.board[y][x] = Figure.name[0] #symb()
+        self.figures_card[Figure.name] = [x, y]
+       
+        return [x, y]
+
+    def clear(self):
+        self.figures_card.clear()
+        self.board = [['0' for i in range(self.SIZE)] for j in range(self.SIZE)]
 
 class ChessMan:
     '''Шахматная фигура'''
-
+    SYMB_UCODE = 0
     BOARD_SIZE = 20
     letter_idx = (
         'A', 'B', 'C', 'D', 'F', 'E', 'G', 'H'
@@ -28,7 +49,14 @@ class ChessMan:
         '1', '2', '3', '4', '5', '6', '7', '8'
     )
 
-    
+    def __init__(self, name:str):
+        self.name = name
+    def __str__(self):
+        return self.name
+
+    def symb(self):
+        return chr(self.SYMB_UCODE)
+
     def field_verify(self, place:str):
         '''Проверка попадания в поле. Возвращает абстрактные координаты'''
         if (
@@ -114,8 +142,8 @@ class ChessMan:
 
 class Castle(ChessMan):
     '''Ладья'''
-    # def __str__(self):
-    #     re
+
+    SYMB_UCODE = 9820
 
     def under_attack(self, place:str):
         '''Список полей атаки ладьи'''
@@ -125,6 +153,8 @@ class Castle(ChessMan):
 
 class Queen(ChessMan):
     '''Ферзь'''
+
+    SYMB_UCODE = 9819
 
     def under_attack(self, place:str):
         '''Список полей атаки ферзя'''
@@ -138,6 +168,8 @@ class Queen(ChessMan):
 
 class Horse(ChessMan):
     '''Лошадь'''
+
+    SYMB_UCODE = 9822
 
     # Куда ходит:
     horse_code = (
@@ -204,35 +236,54 @@ def attack_set_calc(places_set:str, *figures):
 def runner():
 
     input_places_sets = [
-        'D1 D3 E5',
-        'A1 H8 B6',
-        'H7 F8 G6',
-        'F6 E4 H1'
+        # 'D1 D3 E5',
+        # 'A1 H8 B6',
+        # 'H7 F8 G6',
+        'C5 B4 G2'
     ]
+
+    Chessboard1 = Chessboard(10)
+
+    Queen1 = Queen('Queen')
+    Castle1 = Castle('Castle')
+    Horse1 = Horse('Horse')
+
+    figures_set = [Queen1, Castle1, Horse1]
+
+    places_list = input_places_sets[0].split(' ')
+    print(places_list)
+
+    place_idx = [
+        Chessboard1.put_figure(figures_set[i], places_list[i])
+        for i in range(len(places_list))
+    ]
+    print(place_idx)
+    print(Chessboard1.figures_card)
+
+    print('    ',' '.join(Chessboard1.letter_idx))
+    for i in range(Chessboard1.SIZE):
+        print(f'{i+1:{0}{2}}  ',' '.join(Chessboard1.board[i]))
+    print()
+    Chessboard1.clear()
+    print(Chessboard1.figures_card)
+    print('    ',' '.join(Chessboard1.letter_idx))
+    for i in range(Chessboard1.SIZE):
+        print(f'{i+1:{0}{2}}  ',' '.join(Chessboard1.board[i]))
+
+
+
+
+
+    # for inp_pset in input_places_sets:
+    #     places_list = inp_pset.split(' ')
+    #     print(places_list)
+    #     place_ind = []
+    #     for place in places_list[inp_pset]:
+    #         place_ind.append(Chessboard.put_figure(figures_set[place], place))
  
-    Queen1 = Queen()
-    Castle1 = Castle()
-    Horse1 = Horse()
 
-    for inp_set in input_places_sets:
-        attack_set = attack_set_calc(
-            inp_set,
-            Queen1,
-            Castle1,
-            Horse1
-        )
-        print(inp_set)
-        print('Итоговый сет:')
-        print(attack_set)
-        print(len(attack_set))
-        print()
 
-        Chessboard1 = Chessboard(10)
 
-        print(Chessboard1.letter_idx)
-        for i in range(Chessboard1.SIZE):
-            print(' '.join(Chessboard1.board[i]))
-         
 
 if __name__ == '__main__':
     runner()
